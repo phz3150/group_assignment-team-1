832fda8b6244babbf4dbb40de669cc35880078b8
ArtMart1100 <89494210+ArtMart1100@users.noreply.github.com>
2021-11-03 01:10:46 -0400

    Wrote coordinates and sphere_properties functions

--- main_program.py
+++ main_program.py
@@ -2,6 +2,37 @@
 
 import numpy as np
 
+#Function to calculate y-coordinates of the circle
+def coordinates(x,R):
+    """This function calculates the y values of a circle with radius R.\
+    The size of the y array will be double to that of the input x array.
+    Input: Array of x-axis points
+           Radius of circle 
+    Output: Array of y values"""
+    
+    #Numpy is imported from inside function in case it has not been previously imported
+    import numpy as np
+    
+    y = np.sqrt(R**2-x**2)
+    y = np.append(y,-np.sqrt(R**2-x**2))
+    
+    return y
+
+#Function to calculate diameter and surface area of sphere
+def sphere_properties(R):
+    """This function uses the radius of a sphere to calculate its diameter and surface area.
+    Input: Radius [float]
+    Output: [Diameter, Surface Area] [List]"""
+    
+    #Numpy is imported from inside function in case it has not been previously imported
+    import numpy as np
+    
+    #Diameter of sphere
+    D = 2*R
+    #Surface area of sphere
+    SA = 4*np.pi*R**2
+    
+    return [D,SA]
 
 #create array of x coordinates:
 
